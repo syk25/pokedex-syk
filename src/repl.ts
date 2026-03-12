d782fd9d3a4c9460a120c8a26f24db58e88651c8
syk25 <syk2245@gmail.com>
2026-03-13 00:35:36 +0900

    implemented cli repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,27 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
-    const result = input.trim().toLowerCase().split(/\s+/); 
-    return result;
-}  
+  const result = input.trim().toLowerCase().split(/\s+/);
+  return result;
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (line) => {
+    const cleaned = cleanInput(line);
+    if (cleaned.length === 0) {
+      rl.prompt();
+      return;
+    } else {
+      console.log(`Your command was: ${cleaned[0]}`);
+      rl.prompt();
+      return;
+    }
+  });
+}
